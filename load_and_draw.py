2b2e3c3462c087009539949059329f1c3ad218b4
tann <nguyenthanhtank15@gmail.com>
2020-10-20 13:27:44 -0500

    replace kinect by camera video, write more general code

--- load_and_draw.py
+++ load_and_draw.py
@@ -14,7 +14,7 @@ c_handler.setFormatter(logging.Formatter('%(name)s - %(levelname)s - %(message)s
 logger.addHandler(c_handler)
 
 
-def load_and_draw(input_video, input_segmentation, output_dir, region_length):
+def load_and_draw(input_video_path, input_segmentation, output_dir, output_video_name, region_length):
     # create directory for visualization
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
@@ -22,28 +22,31 @@ def load_and_draw(input_video, input_segmentation, output_dir, region_length):
     seg = pd.read_csv(input_segmentation)
     # select relevant annotations for the video
     # one video can have multiple annotations
-    seg = seg[seg['movie1'] == os.path.splitext(os.path.basename(input_video))[0]]
-    # pick one of the annotations
-    seg_points = seg['segment1'][0]
-    seg_points = seg_points.split('\n')
-    seg_points = [x.strip().split(' ') for x in seg_points]
-    seg_points = [float(x[1]) for x in seg_points if 'BreakPoint' in x]
+    seg = seg[seg['movie1'] == os.path.splitext(os.path.basename(input_video_path))[0]]
+    # parse annotations, from string to a list of breakpoints for each annotation
+    seg['segment_processed'] = seg['segment1']
+    seg['segment_processed'] = seg['segment_processed'].apply(lambda s: s.split('\n'))
+    seg['segment_processed'] = seg['segment_processed'].apply(lambda l: [float(e.split(' ')[1]) for e in l
+                                                                         if 'BreakPoint' in e])
     # initialize video_read and video_write streams
-    cap = cv2.VideoCapture(input_video)
+    cap = cv2.VideoCapture(input_video_path)
     if cap.isOpened() is False:
         logger.error("Error opening video stream or file")
-    ret, frame = cap.read()
     height, width = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT)), int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
     fps = cap.get(cv2.CAP_PROP_FPS)
     fourcc = cv2.VideoWriter_fourcc(*'XVID')
-    output_video = os.path.join(output_dir, 'output_video.avi')
-    out = cv2.VideoWriter(output_video, fourcc=fourcc, fps=fps, frameSize=(width, height))
-    # calculate segmentation points according to fps
-    seg_points = [int(x * fps) for x in seg_points]
+    output_video_name = os.path.join(output_dir, output_video_name)
+    out = cv2.VideoWriter(output_video_name, fourcc=fourcc, fps=fps, frameSize=(width, height))
+    # calculate segmentation points according to fps, consider only one annotation
+    condition = seg['condition'].iloc[0]
+    seg_points = seg['segment_processed'].values[0]
+    seg_points = np.array(seg_points)
+    seg_points = (seg_points * fps).astype(np.int)
     # extracting frames while adding annotations to frames
     frame_id = 0
     buffer = dict()
     while cap.isOpened():
+        ret, frame = cap.read()
         if ret is True:
             frame_id += 1
             # cv2.imshow('frame', frame)
@@ -51,15 +54,16 @@ def load_and_draw(input_video, input_segmentation, output_dir, region_length):
             #     break
             cv2.putText(frame, f'Frame: {frame_id}', org=(50, 50), fontFace=cv2.FONT_HERSHEY_SIMPLEX,
                         fontScale=1, color=(255, 0, 0))
+            cv2.putText(frame, f'Condition: {condition}', org=(50, 100), fontFace=cv2.FONT_HERSHEY_SIMPLEX,
+                        fontScale=1, color=(255, 0, 0))
             if frame_id in seg_points:
-                cv2.putText(frame, f'SEGMENTED', org=(50, 100), fontFace=cv2.FONT_HERSHEY_SIMPLEX,
+                cv2.putText(frame, f'SEGMENTED', org=(50, 150), fontFace=cv2.FONT_HERSHEY_SIMPLEX,
                             fontScale=1, color=(0, 255, 0))
                 # cv2.imwrite(f'{frame_id}.jpg', frame)
             buffer[frame_id] = frame
             out.write(frame)
         else:
             break
-        ret, frame = cap.read()
 
     cap.release()
     out.release()
@@ -76,8 +80,9 @@ def load_and_draw(input_video, input_segmentation, output_dir, region_length):
 
 
 if __name__ == "__main__":
-    logger.info('Input video: data/small_videos/4.3.1_kinect_trim.mp4')
+    logger.info('Input video: data/small_videos/6.2.5_C1_trim.mp4')
     logger.info('Input segmentation: database.200731.1.csv')
     logger.info('Output dir: output')
-    load_and_draw(input_video='data/small_videos/4.3.1_kinect_trim.mp4',
-                  input_segmentation='database.200731.1.csv', output_dir='output', region_length=60)
+    load_and_draw(input_video_path='data/small_videos/6.2.5_C1_trim.mp4',
+                  input_segmentation='database.200731.1.csv', output_dir='output',
+                  output_video_name='output_video.avi', region_length=60)
