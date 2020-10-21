2ffd81bf6bc70686e81ec901f665636cf4235d40
tann <nguyenthanhtank15@gmail.com>
2020-10-20 20:45:30 -0500

    refactor, add comments

--- load_and_draw.py
+++ load_and_draw.py
@@ -4,7 +4,7 @@ import cv2
 import os
 import logging
 from shutil import rmtree
-
+from typing import List
 # Set-up logger
 logger = logging.getLogger(__name__)
 logger.setLevel(os.environ.get('LOGLVL', logging.INFO))
@@ -12,6 +12,12 @@ logger.setLevel(os.environ.get('LOGLVL', logging.INFO))
 c_handler = logging.StreamHandler()
 c_handler.setFormatter(logging.Formatter('%(name)s - %(levelname)s - %(message)s'))
 logger.addHandler(c_handler)
+
+
+def string_to_segments(raw_string: str) -> List:
+    raw_string = raw_string.split('\n')
+    list_segments = [float(x.split(' ')[1]) for x in raw_string if 'BreakPoint' in x]
+    return list_segments
 
 
 def load_and_draw(input_video_path, input_segmentation, output_dir, output_video_name, region_length):
@@ -25,9 +31,7 @@ def load_and_draw(input_video_path, input_segmentation, output_dir, output_video
     seg = seg[seg['movie1'] == os.path.splitext(os.path.basename(input_video_path))[0]]
     # parse annotations, from string to a list of breakpoints for each annotation
     seg['segment_processed'] = seg['segment1']
-    seg['segment_processed'] = seg['segment_processed'].apply(lambda s: s.split('\n'))
-    seg['segment_processed'] = seg['segment_processed'].apply(lambda l: [float(e.split(' ')[1]) for e in l
-                                                                         if 'BreakPoint' in e])
+    seg['segment_processed'] = seg['segment_processed'].apply(string_to_segments)
     # initialize video_read and video_write streams
     cap = cv2.VideoCapture(input_video_path)
     if cap.isOpened() is False:
@@ -37,7 +41,8 @@ def load_and_draw(input_video_path, input_segmentation, output_dir, output_video
     fourcc = cv2.VideoWriter_fourcc(*'XVID')
     output_video_name = os.path.join(output_dir, output_video_name)
     out = cv2.VideoWriter(output_video_name, fourcc=fourcc, fps=fps, frameSize=(width, height))
-    # calculate segmentation points according to fps, consider only one annotation
+    # calculate segmentation points according to fps
+    # consider only one annotation
     condition = seg['condition'].iloc[0]
     seg_points = seg['segment_processed'].values[0]
     seg_points = np.array(seg_points)
@@ -52,16 +57,18 @@ def load_and_draw(input_video_path, input_segmentation, output_dir, output_video
             # cv2.imshow('frame', frame)
             # if cv2.waitKey(25) & 0xFF == ord('q'):
             #     break
+            # Add frame_id and condition (coarse or fine)
             cv2.putText(frame, f'Frame: {frame_id}', org=(50, 50), fontFace=cv2.FONT_HERSHEY_SIMPLEX,
                         fontScale=1, color=(255, 0, 0))
             cv2.putText(frame, f'Condition: {condition}', org=(50, 100), fontFace=cv2.FONT_HERSHEY_SIMPLEX,
                         fontScale=1, color=(255, 0, 0))
             if frame_id in seg_points:
+                # Add tag for the segmented frame
                 cv2.putText(frame, f'SEGMENTED', org=(50, 150), fontFace=cv2.FONT_HERSHEY_SIMPLEX,
                             fontScale=1, color=(0, 255, 0))
                 # cv2.imwrite(f'{frame_id}.jpg', frame)
-            buffer[frame_id] = frame
             out.write(frame)
+            buffer[frame_id] = frame
         else:
             break
 
@@ -82,7 +89,7 @@ def load_and_draw(input_video_path, input_segmentation, output_dir, output_video
 if __name__ == "__main__":
     logger.info('Input video: data/small_videos/6.2.5_C1_trim.mp4')
     logger.info('Input segmentation: database.200731.1.csv')
-    logger.info('Output dir: output')
+    logger.info('Output dir: output/')
     load_and_draw(input_video_path='data/small_videos/6.2.5_C1_trim.mp4',
                   input_segmentation='database.200731.1.csv', output_dir='output',
                   output_video_name='output_video.avi', region_length=60)
