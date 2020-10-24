100bb46501e02367ebbd7be3615c1673b2b1af37
tann <nguyenthanhtank15@gmail.com>
2020-10-23 23:41:59 -0500

    refactor to classes, embed segmentations to video

--- load_and_draw.py
+++ load_and_draw.py
@@ -1,10 +1,19 @@
 import numpy as np
 import pandas as pd
+from matplotlib.backends.backend_agg import FigureCanvasAgg
+from matplotlib.figure import Figure
+import seaborn as sns
 import cv2
 import os
 import logging
 from shutil import rmtree
-from typing import List
+import skvideo
+
+ffmpeg_path = 'C:/Users/nguye/ffmpeg-4.3.1-2020-10-01-full_build/bin'
+skvideo.setFFmpegPath(ffmpeg_path)
+import skvideo.io
+from utils import CV2VideoReader, CV2VideoWriter, SegmentationVideo, Sample
+
 # Set-up logger
 logger = logging.getLogger(__name__)
 logger.setLevel(os.environ.get('LOGLVL', logging.INFO))
@@ -14,76 +23,87 @@ c_handler.setFormatter(logging.Formatter('%(name)s - %(levelname)s - %(message)s
 logger.addHandler(c_handler)
 
 
-def string_to_segments(raw_string: str) -> List:
-    raw_string = raw_string.split('\n')
-    list_segments = [float(x.split(' ')[1]) for x in raw_string if 'BreakPoint' in x]
-    return list_segments
+def draw_on_canvas(seg_points) -> np.ndarray:
+    fig = Figure()
+    canvas = FigureCanvasAgg(fig)
+    # Do some plotting.
+    ax = fig.add_subplot(311)
+    ax1 = fig.add_subplot(312)
+    ax2 = fig.add_subplot(313)
+    # sns.violinplot(data=seg_points, orient='h', ax=ax)
+    sns.swarmplot(data=seg_points, orient='h', ax=ax, alpha=.1)
+    sns.histplot(data=seg_points, bins=100)
+    # sns.stripplot(data=seg_points, orient='h', ax=ax1, alpha=.1)
+    ax2.vlines(seg_points, ymin=0, ymax=1, alpha=0.05)
+    canvas.draw()
+    img = cv2.cvtColor(np.asarray(canvas.buffer_rgba()), cv2.COLOR_RGBA2BGR)
+    canvas_resized = cv2.resize(
+        img[:, int(img.shape[1] * 0.145): int(img.shape[1] * 0.88), :], (960, 200))
+    return canvas_resized
 
 
-def load_and_draw(input_video_path, input_segmentation, output_dir, output_video_name, region_length):
+def load_and_draw(input_video_path, input_segmentation, output_dir, output_video_name):
     # create directory for visualization
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
     # load segmentation data
-    seg = pd.read_csv(input_segmentation)
-    # select relevant annotations for the video
-    # one video can have multiple annotations
-    seg = seg[seg['movie1'] == os.path.splitext(os.path.basename(input_video_path))[0]]
-    # parse annotations, from string to a list of breakpoints for each annotation
-    seg['segment_processed'] = seg['segment1']
-    seg['segment_processed'] = seg['segment_processed'].apply(string_to_segments)
+    data_frame = pd.read_csv(input_segmentation)
+    seg_video = SegmentationVideo(data_frame=data_frame, video_path=input_video_path)
     # initialize video_read and video_write streams
-    cap = cv2.VideoCapture(input_video_path)
-    if cap.isOpened() is False:
-        logger.error("Error opening video stream or file")
-    height, width = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT)), int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-    fps = cap.get(cv2.CAP_PROP_FPS)
-    fourcc = cv2.VideoWriter_fourcc(*'VP90')
-    output_video_name = os.path.join(output_dir, output_video_name)
-    out = cv2.VideoWriter(output_video_name, fourcc=fourcc, fps=fps, frameSize=(width, height))
+    cv2_video_reader = CV2VideoReader(input_video_path=input_video_path)
+    output_video_path = os.path.join(output_dir, output_video_name)
+    sk_video_writer = skvideo.io.FFmpegWriter(output_video_path)
+    # cv2_video_writer = CV2VideoWriter(output_video_path=output_video_path)
     # calculate segmentation points according to fps
     # consider only one annotation
-    condition = seg['condition'].iloc[0]
-    seg_points = seg['segment_processed'].values[0]
-    seg_points = np.array(seg_points)
-    seg_points = (seg_points * fps).astype(np.int)
+    condition = 'coarse'
+    seg_points = seg_video.get_segments(n_annotators=100, condition=condition)
+    seg_points = np.hstack(seg_points)
+    seg_points = (seg_points * cv2_video_reader.fps).astype(np.int)
+    # get canvas drawn with segmentation points
+    canvas_img = draw_on_canvas(seg_points)
     # extracting frames while adding annotations to frames
+    logger.info('Processing video...')
     frame_id = 0
-    buffer = dict()
-    while cap.isOpened():
-        ret, frame = cap.read()
+    while cv2_video_reader.capture.isOpened():
+        ret, frame = cv2_video_reader.capture.read()
         if ret is True:
             frame_id += 1
             # Add frame_id and condition (coarse or fine)
-            cv2.putText(frame, f'Frame: {frame_id}', org=(50, 50), fontFace=cv2.FONT_HERSHEY_SIMPLEX,
+            cv2.putText(frame, f'Frame: {frame_id}', org=(50, 50),
+                        fontFace=cv2.FONT_HERSHEY_SIMPLEX,
                         fontScale=1, color=(255, 0, 0))
-            cv2.putText(frame, f'Condition: {condition}', org=(50, 100), fontFace=cv2.FONT_HERSHEY_SIMPLEX,
+            cv2.putText(frame, f'Condition: {condition}', org=(50, 100),
+                        fontFace=cv2.FONT_HERSHEY_SIMPLEX,
                         fontScale=1, color=(255, 0, 0))
             if frame_id in seg_points:
                 # Add tag for the segmented frame
-                cv2.putText(frame, f'SEGMENTED', org=(50, 150), fontFace=cv2.FONT_HERSHEY_SIMPLEX,
+                cv2.putText(frame, f'SEGMENTED', org=(50, 150),
+                            fontFace=cv2.FONT_HERSHEY_SIMPLEX,
                             fontScale=1, color=(0, 255, 0))
-                # cv2.imwrite(f'{frame_id}.jpg', frame)
-            out.write(frame)
+            frame = np.concatenate((frame, canvas_img), axis=0)
+            # cv2_video_writer.writer.write(frame)
+            sk_video_writer.writeFrame(frame[:, :, ::-1])
+            # Real-time showing video
             # cv2.imshow('frame', frame)
             # if cv2.waitKey(25) & 0xFF == ord('q'):
             #     break
-            buffer[frame_id] = frame
         else:
             break
 
-    cap.release()
-    out.release()
-    cv2.destroyAllWindows()
+    logger.info('Done load_and_draw!')
 
+
+def draw_images():
+    pass
     # drawing segmented regions from buffer
-    for seg_point in seg_points:
-        logger.debug(f'Segmented frame_id: {seg_point}')
-        if os.path.exists(f'{output_dir}/{seg_point}'):
-            rmtree(f'{output_dir}/{seg_point}')
-        os.makedirs(f'{output_dir}/{seg_point}')
-        for i in range(seg_point - region_length, seg_point + region_length, 3):
-            cv2.imwrite(f'{output_dir}/{seg_point}/{i}.jpg', buffer[i])
+    # for seg_point in seg_points:
+    #     logger.debug(f'Segmented frame_id: {seg_point}')
+    #     if os.path.exists(f'{output_dir}/{seg_point}'):
+    #         rmtree(f'{output_dir}/{seg_point}')
+    #     os.makedirs(f'{output_dir}/{seg_point}')
+    #     for i in range(seg_point - region_length, seg_point + region_length, 3):
+    #         cv2.imwrite(f'{output_dir}/{seg_point}/{i}.jpg', buffer[i])
 
 
 if __name__ == "__main__":
@@ -92,4 +112,4 @@ if __name__ == "__main__":
     logger.info('Output dir: output/')
     load_and_draw(input_video_path='data/small_videos/6.2.5_C1_trim.mp4',
                   input_segmentation='database.200731.1.csv', output_dir='output',
-                  output_video_name='output_video.mp4', region_length=60)
+                  output_video_name='output_video.mp4')
---
+++ utils.py
@@ -0,0 +1,90 @@
+import numpy as np
+import cv2
+import os
+import logging
+from typing import List, Dict
+
+# Set-up logger
+logger = logging.getLogger(__name__)
+logger.setLevel(os.environ.get('LOGLEVEL', logging.INFO))
+# must have a handler, otherwise logging will use lastresort
+c_handler = logging.StreamHandler()
+c_handler.setFormatter(logging.Formatter('%(name)s - %(levelname)s - %(message)s'))
+logger.addHandler(c_handler)
+
+
+class Sample:
+    sample_dict = {'1': 1, '10': 10, '2': 2}
+    sample_list = [1, 5, 7]
+    sample_list_of_lists = [[1, 2], [3, 4], [5, 6]]
+    sample_tuple = (1, 3)
+    sample_2d_array = np.array([[1, 2, 3], [4, 5, 6]])
+    sample_list_of_array = [np.array([1, 2]), np.array([3, 4, 5])]
+
+
+class SegmentationVideo:
+    def __init__(self, data_frame, video_path):
+        self.data_frame = data_frame[
+            data_frame['movie1'] == os.path.splitext(os.path.basename(video_path))[0]]
+
+    @staticmethod
+    def string_to_segments(raw_string: str) -> np.ndarray:
+        raw_string = raw_string.split('\n')
+        list_of_segments = [float(x.split(' ')[1]) for x in raw_string if 'BreakPoint' in x]
+        list_of_segments = np.array(list_of_segments)
+        return list_of_segments
+
+    def get_segments(self, n_annotators=1, condition='coarse') -> List:
+        seg = self.data_frame[self.data_frame['condition'] == condition]
+        # parse annotations, from string to a list of breakpoints for each annotation
+        seg_processed = seg['segment1'].apply(SegmentationVideo.string_to_segments)
+        seg_points = seg_processed.values[:n_annotators]
+        return seg_points
+
+
+class CV2VideoReader:
+    def __init__(self, input_video_path):
+        logger.debug('Creating an instance of CV2VideoReader')
+        self.capture = cv2.VideoCapture(input_video_path)
+        if self.capture.isOpened() is False:
+            logger.error("Error opening video stream for reading")
+        self.height, self.width = int(self.capture.get(cv2.CAP_PROP_FRAME_HEIGHT)), int(
+            self.capture.get(cv2.CAP_PROP_FRAME_WIDTH))
+        self.fps = self.capture.get(cv2.CAP_PROP_FPS)
+
+    def get_cv2_video_reader(self) -> Dict:
+        return {'reader': self.capture,
+                'height': self.height,
+                'width': self.width,
+                'fps': self.fps}
+
+    def __del__(self):
+        logger.debug('Destroying an instance of CV2VideoReader')
+        self.capture.release()
+
+
+class CV2VideoWriter:
+    def __init__(self, output_video_path, fps=30, height=740, width=960):
+        logger.debug('Creating an instance of CV2VideoWriter')
+        if os.path.splitext(output_video_path)[1] == '.avi':
+            self.fourcc = cv2.VideoWriter_fourcc(*'XVID')
+        elif os.path.splitext(output_video_path)[1] == '.mp4':
+            self.fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+        else:
+            logger.error(f'Error opening video stream for writing \n'
+                         f'Incorrect output format: {os.path.splitext(output_video_path)[1]}')
+        self.writer = cv2.VideoWriter(output_video_path, fourcc=self.fourcc, fps=fps,
+                                      frameSize=(width, height))
+        self.fps = fps
+        self.height = height
+        self.width = width
+
+    def get_cv2_video_writer(self) -> Dict:
+        return {'writer': self.writer,
+                'fps': self.fps,
+                'height': self.height,
+                'width': self.width}
+
+    def __del__(self):
+        logger.debug('Destroying an instance of CV2VideoWriter')
+        self.writer.release()
