e80d0b41c6da288662df4eeecd385344162a9b2f
Matt Bezdek <mbezdek@wustl.edu>
2020-11-02 16:01:44 -0600

    fix pysot pythonpath

--- utils.py
+++ utils.py
@@ -8,9 +8,9 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 from typing import List, Dict
 import torch
-from pysot.core.config import cfg
-from pysot.models.model_builder import ModelBuilder
-from pysot.tracker.tracker_builder import build_tracker
+from pysot.pysot.core.config import cfg
+from pysot.pysot.models.model_builder import ModelBuilder
+from pysot.pysot.tracker.tracker_builder import build_tracker
 import argparse
 import configparser
 
